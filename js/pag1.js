006e674effd532df1ef18419cfe7013f56ba0132
Nazzareno Pasqual <79537141+TerribleCoding@users.noreply.github.com>
2021-04-10 22:28:47 +0200

    Updated code and display of pictures
    Refactored css image classes, corrected parents in js, added Flexbox

--- js/pag1.js
+++ js/pag1.js
@@ -1,8 +1,3 @@
-var container;
-var container1;
-var container2;
-var container3;
-var container4;
 var Ve = [];
 var Bb = [];
 var Cl = [];
@@ -11,7 +6,6 @@ var Ps = [];
 
 function setup() {
     noCanvas();
-    container = document.getElementById('City1');
     Ve = [
         '../imagine/Pag 1/City/City1/DSC_0150.jpg',
         '../imagine/Pag 1/City/City1/DSC_0150_1.jpg',
@@ -23,7 +17,7 @@ function setup() {
         let link = createA(Ve[i], '');
         //link.addClass('Ve');
         link.attribute('data-lightbox', 'Ve');
-        link.parent(container);
+        link.parent(document.getElementById('City1'));
         let imm = createImg(Ve[i], '');
         if (i < 1) {
             imm.addClass('Ve1');
@@ -32,7 +26,6 @@ function setup() {
         }
         imm.parent(link);
     }
-    container1 = document.getElementById('City2');
     Bb = [
         '../imagine/Pag 1/City/City2/DSC_0150.jpg',
         '../imagine/Pag 1/City/City2/DSC_0150_1.jpg',
@@ -44,7 +37,7 @@ function setup() {
         let link = createA(Bb[i], '');
         //link.addClass('Bb');
         link.attribute('data-lightbox', 'Bb');
-        link.parent(container);
+        link.parent(document.getElementById('City2'));
         let imm = createImg(Bb[i], '');
         if (i < 1) {
             imm.addClass('Bb1');
@@ -53,7 +46,6 @@ function setup() {
         }
         imm.parent(link);
     }
-    container2 = document.getElementById('City3');
     Cl = [
         '../imagine/Pag 1/City/City3/DSC_0150.jpg',
         '../imagine/Pag 1/City/City3/DSC_0150_1.jpg',
@@ -65,7 +57,7 @@ function setup() {
         let link = createA(Cl[i], '');
         //link.addClass('Ve');
         link.attribute('data-lightbox', 'Cl');
-        link.parent(container);
+        link.parent(document.getElementById('City3'));
         let imm = createImg(Cl[i], '');
         if (i < 1) {
             imm.addClass('Cl1');
@@ -74,20 +66,18 @@ function setup() {
         }
         imm.parent(link);
     }
-    container3 = document.getElementById('Sunset');
     St = [
         '../imagine/Pag 1/Sunset/DSC_0150.jpg',
         '../imagine/Pag 1/Sunset/DSC_0150_1.jpg',
         '../imagine/Pag 1/Sunset/DSC_0150_2.jpg',
         '../imagine/Pag 1/Sunset/DSC_0150_4.jpg',
         '../imagine/Pag 1/Sunset/DSC_0150_5.jpg'
-
     ];
     for (let i = 0; i < St.length; i++) {
         let link = createA(St[i], '');
         //link.addClass('Ve');
         link.attribute('data-lightbox', 'St');
-        link.parent(container);
+        link.parent(document.getElementById('Sunset'));
         let imm = createImg(St[i], '');
         if (i < 1) {
             imm.addClass('St1');
@@ -96,20 +86,18 @@ function setup() {
         }
         imm.parent(link);
     }
-    container4 = document.getElementById('Pets');
     Ps = [
         '../imagine/Pag 1/Sunset/DSC_0150.jpg',
         '../imagine/Pag 1/Sunset/DSC_0150_1.jpg',
         '../imagine/Pag 1/Sunset/DSC_0150_2.jpg',
         '../imagine/Pag 1/Sunset/DSC_0150_4.jpg',
         '../imagine/Pag 1/Sunset/DSC_0150_5.jpg'
-
     ];
     for (let i = 0; i < Ps.length; i++) {
         let link = createA(Ps[i], '');
         //link.addClass('Ve');
         link.attribute('data-lightbox', 'Ps');
-        link.parent(container);
+        link.parent(document.getElementById('Pets'));
         let imm = createImg(Ps[i], '');
         if (i < 1) {
             imm.addClass('Ps1');
